486ee49235f69925307da506229c33a81cb3d827
Pedro Calil <pedrocalil.cordeiro@gmail.com>
2025-09-22 18:59:55 -0300

    fix(render): listen on process.env.PORT and host 0.0.0.0

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,13 @@ import { env } from "./env";
 
 app
   .listen({
-    port: env.PORT,
+    port: env.PORT, // usa a porta do Render
+    host: "0.0.0.0", // obrigatório no Render
   })
   .then(() => {
-    console.log("HTTP server is running!");
+    console.log(`HTTP server is running on ${env.PORT}`);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
   });
